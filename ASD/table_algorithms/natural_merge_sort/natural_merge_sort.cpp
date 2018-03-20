67f08e4fe3b1606d6c7a7f0c232a36882c05d478
jakubdybczak <jakub.dybczak@gmail.com>
2018-03-20 21:16:56 +0100

    Fixed natural merge sort

--- ASD/table_algorithms/natural_merge_sort/natural_merge_sort.cpp
+++ ASD/table_algorithms/natural_merge_sort/natural_merge_sort.cpp
@@ -1,83 +1,58 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ctime>
-#include <limits.h>
-/**
-	Swaps array elements with indexes "i" and "j"
-	
-	@param A[] - array
-	@param i - index of first element
-	@param j - index of second element
-*/
-void swap(int A[], int i, int j){
-	int temp = A[i];
-	A[i] = A[j];
-	A[j] = temp;
-}
 
-
-
-/**
-	Merges sorted sub-arrays. Based on Cormen's implementation.
-	
-	@param A[] - array
-	@param p - index of first element
-	@param q - index of middle element
-	@param r - index of last element
-*/
-void merge(int A[], int p, int q, int r){
-	int n1 = q - p + 1;
-	int n2 = r - q;
-	int left[n1+1];
-	int right[n2+1];
-	for(int i = 0; i < n1; i++){
-		left[i] = A[p+i];
-	}
-	
-	for(int i = 0; i < n2; i++){
-		right[i] = A[q+i+1];
-	}
-	
-	left[n1]  = INT_MAX;
-	right[n2] = INT_MAX;
-	int i = 0, j = 0;
-	for(int k = p; k <= r;k++){
-		if(left[i] <= right[j]){
-			A[k] = left[i++];
-		}
-		else{
-			A[k] = right[j++];
-		}
-	}
-}
-
-
-/**
-	Sorts array. Based on Cormen's implementation.
-	
-	@param A[] - array
-	@param p - index of first element
-	@param r - index of last element
-*/
-void mergeSort(int A[], int p, int r){
-	if(p < r){
-		int q = (p+r)/2;
-		mergeSort(A, p, q);
-		mergeSort(A, q+1, r);
-		merge(A, p, q, r);
-	}
-}
-
-/**
-	Prints whole array
-	
-	@param A[] - array
-	@param n - size of array
-*/
 void printArray(int A[], int n){
 	for(int i = 0; i < n; i++){
 		std::cout << A[i] << " ";
 	}
+	std::cout << std::endl;
+}
+int find_ser(int t[], int j, int n){
+	for(int i=j+1; i<n; i++){
+		if(t[i] < t[i-1]){
+			return i;
+		}
+	}
+	return n;
+}
+void merge(int t1[], int t2[], int p1, int k1, int p2, int k2){
+	int i = p1;
+	int i1 = p1;
+	int i2 = p2;
+	while(i1 < k1 && i2 < k2){
+		if(t1[i1] < t1[i2]){
+			t2[i++]=t1[i1++];
+		}
+		else {
+			t2[i++]=t1[i2++];
+		}
+	}
+	while(i1 < k1) t2[i++]=t1[i1++];
+	while(i2 < k2) t2[i++]=t1[i2++];
+}
+void natural_merge_sort(int t[], int n){
+	int *t2 = new int[n];
+	int p1,p2,k1,k2;
+	p1=0;
+	
+	while(true){
+		k1=find_ser(t,p1,n);
+		if(p1==0 && k1==n) break;
+		p2=k1;
+		if(p2==n) k2=n; else k2 = find_ser(t,p2,n);
+		merge(t,t2,p1,k1,p2,k2);
+		if(k2 == n){
+			for(int i = 0; i < n; i++){
+				t[i]=t2[i];
+			}
+			p1=0;
+		}
+		else {
+			p1=k2;
+		}
+	}
+	delete [] t2;
 }
 int main(int argc, char** argv) {
 	srand(time(NULL));
@@ -88,12 +63,12 @@ int main(int argc, char** argv) {
 	for(int i = 0; i < n; i++){
 		tab[i]=rand();
 	}
-	std::cout << std::endl << "ARRAY" << std::endl;
-
+	std::cout << std::endl << "ARRAY " << std::endl;
 	printArray(tab,n);
 
 	std::cout << std::endl << "SORTED ARRAY" << std::endl;
-	mergeSort(tab,0,n-1);
+	natural_merge_sort(tab,n);
 	printArray(tab,n);
 	return 0;
 }
+
